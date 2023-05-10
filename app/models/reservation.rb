3a3ac392d12f9619a1618ebccadb077939d22e36
Harrison Njuguna <noqsnoqs@gmail.com>
2023-05-10 11:43:21 +0300

    Add migration files and models

---
+++ app/models/reservation.rb
@@ -0,0 +1,8 @@
+class Reservation < ApplicationRecord
+    belongs_to :user
+    belongs_to :twowheeler
+   
+    validates :city, presence: true
+    validates :duration_of_test_drive, presence: true
+    validates :date_of_reservation, presence: true
+end
