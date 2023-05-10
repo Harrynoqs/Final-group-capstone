50c68fc300a0b1bdedf2477a48133423f3630adb
Harrison Njuguna <noqsnoqs@gmail.com>
2023-05-10 11:50:38 +0300

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.create(name: 'Harry')
+User.create(name: 'Vivian')
+User.create(name: 'Ellon')
+User.create(name: 'Noqs')
+User.create(name: 'Soulemanou')
+User.create(name: 'john')
+User.create(name: 'Raphael')
+User.create(name: 'Liam')
+
+
+Twowheeler.create(name: "Honda CB Shine", description: "A popular commuter motorcycle from Honda with sleek styling and reliable performance.", image_url: "https://images.unsplash.com/photo-1597919169417-6da890b6cf8b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8aG9uZGElMjBjYiUyMHNoaW5lfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80", price: 10000,model_year: "2021",  engine_type: "Single-cylinder, 4-stroke, air-cooled", fuel_type: "Petrol")
+Twowheeler.create(name: "Vespa Primavera 150", description: "A classic Italian scooter with stylish design and great performance.", image_url: "https://images.unsplash.com/photo-1620638109346-c1d2b1e047f9?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8dmVzcGF8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80", price: 440000, model_year: "2021", engine_type: "Single-cylinder, 4-stroke, air-cooled", fuel_type: "Petrol")
+Twowheeler.create(name: "Harley-Davidson Street 750", description: "An entry-level cruiser motorcycle from Harley-Davidson with an iconic American style and smooth performance.", image_url: "https://images.unsplash.com/photo-1533107299103-8e8f0da87e64?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzR8fGhhcmxleSUyZGlzZmFjdG9yeXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80", price: 600000, model_year: "2021", engine_type: "Liquid-cooled, Revolution X™ V-Twin", fuel_type: "Petrol")
+Twowheeler.create(name: "Yamaha YZF-R1", description: "A high-performance sports motorcycle from Yamaha with a race-inspired design and cutting-edge technology.", image_url: "https://images.unsplash.com/photo-1580824671613-ee3b6df69bb7?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8eWFtYmF8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80", price: 1320000, model_year: "2021", engine_type: "Liquid-cooled, 4-stroke, DOHC, 4-valve", fuel_type: "Petrol")
+Twowheeler.create(name: "Yamaha YZF-R6", description: "A popular sportbike", image_url: "https://cdn.otopark.com/wp-content/uploads/2016/10/2017_Yamaha_YZF-R6_28.jpg", price: 13199.0, model_year: "2022", engine_type: "599cc inline-four", fuel_type: "Gasoline") 
+Twowheeler.create(name: "KTM 390 Duke", description: "A powerful and agile naked motorcycle from KTM, known for its sharp handling and impressive performance.", image_url: "https://images.unsplash.com/photo-1608745243922-860b040834a5?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8a3RtJTIzOTAncyUyMGR1a2V8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80", price: 444000, model_year: "2021", engine_type: "Single-cylinder, 4-stroke, liquid-cooled", fuel_type: "Petrol")
+Twowheeler.create(name: "BMW S 1000 RR", description: "A high-performance sports motorcycle from BMW with advanced technology and excellent handling.", image_url: "https://images.unsplash.com/photo-1575372643242-e7f42af173a9?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGJtdyUyMGElMjBzJTIwMTAwMCUyMHJyJTIwc2hpcHBpbmclMjByZXNpZGVudHN8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80", price: 1400000, model_year: "2021", engine_type: "Liquid-cooled, 4-stroke, DOHC, 4-valve", fuel_type: "Petrol")
+Twowheeler.create(name: "Suzuki Burgman Street", description: "A comfortable and practical scooter from Suzuki, designed for urban commuting and touring.", image_url: "https://images.unsplash.com/photo-1622760908427-40d8a9c667f6?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fHN1a2lkaSUyMGJ1cmdtYW58ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80", price: 284000, model_year: "2021", engine_type: "Single-cylinder, 4-stroke, air-cooled", fuel_type: "Petrol")
+Twowheeler.create(name: "Ducati Panigale V4", description: "A premium sports motorcycle from Ducati, featuring a powerful engine and advanced electronics for exceptional performance.", image_url: "https://images.unsplash.com/photo-1603448349264-6491c0d4ec91?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGR1Y2F0aSUyMHBhbmlnYWxlJTIwdjQlMjBwaWVjZXN0fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80", price: 2040000, model_year: "2021", engine_type: "Liquid-cooled, 4-stroke, DOHC, 4-valve", fuel_type: "Petrol")
+Twowheeler.create(name: "Kawasaki Ninja ZX-10R", description: "A powerful and aerodynamic sportbike.", image_url: "https://www.motosikletsitesi.com/wp-content/uploads/2022/10/23MY_Ninja-ZX-10R-Performance_GN1_Front.001.png", price: 0.16999e5, model_year: "2022", engine_type: "998cc inline-four", fuel_type: "Gasoline")
+
+
+
+Reservation.create(user_id: 1, twowheeler_id: 1, city: 'Nairobi', duration_of_test_drive: 1, date_of_reservation: '2023-03-15' )
+Reservation.create(user_id: 2, twowheeler_id: 2, city: 'Mombasa', duration_of_test_drive: 1, date_of_reservation: '2023-03-25')
+Reservation.create(user_id: 3,twowheeler_id: 3, city: 'Malindi', duration_of_test_drive: 2, date_of_reservation: '2023-03-20')
+Reservation.create(user_id: 4, twowheeler_id: 4, city: 'Nakuru', duration_of_test_drive: 1, date_of_reservation: '2023-04-15')
+Reservation.create(user_id: 5, twowheeler_id: 5, city: 'Kisumu', duration_of_test_drive: 1, date_of_reservation: '2023-04-22' )
+Reservation.create(user_id: 6, twowheeler_id: 6, city: 'Dar', duration_of_test_drive: 2, date_of_reservation: '2023-04-25')
+Reservation.create(user_id: 7,twowheeler_id: 7, city: 'Voi', duration_of_test_drive: 1, date_of_reservation: '2023-04-20')
+Reservation.create(user_id: 8, twowheeler_id: 8, city: 'Machakos', duration_of_test_drive: 1, date_of_reservation: '2023-04-15')
