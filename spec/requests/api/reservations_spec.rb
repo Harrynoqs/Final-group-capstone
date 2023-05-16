ad76b7eb6145bd05c27459ca5c9a5fef2e3ee629
Soulemanou Kunchiefuh Babanou <kunchiefuh@gmail.com>
2023-05-16 22:32:56 +0200

    modified the reservation spec

--- spec/requests/api/reservations_spec.rb
+++ spec/requests/api/reservations_spec.rb
@@ -15,4 +15,22 @@ describe 'Reservations API' do
           twowheeler_id: { type: :integer }
 
         },
-        
+        required: %w[city,  duration_of_test_drive, date_of_reservation,  user_id, twowheeler_id]
+      }
+
+      response '201', 'reservation created' do
+        let(:reservation) do
+          { city: 'Milan', date_of_reservation: '2023-05-17 00:00:00', duration_of_test_drive: 130,  user_id: 1, twowheeler_id: 1 }
+        end
+        run_test!
+      end
+
+      response '422', 'invalid request' do
+        let(:reservation) do
+           { city: 'Milan', duration_of_test_drive: 130,  user_id: 1, twowheeler_id: 1 }
+        end
+        run_test!
+      end
+    end
+  end
+end
