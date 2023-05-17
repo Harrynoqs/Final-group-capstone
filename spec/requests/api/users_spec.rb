e08b0e41c23be7fe65279af7ec6c17b5554f1684
Soulemanou Kunchiefuh Babanou <kunchiefuh@gmail.com>
2023-05-17 14:21:36 +0200

    added the user api spec

--- spec/requests/api/users_spec.rb
+++ spec/requests/api/users_spec.rb
@@ -1,4 +1,50 @@
 require 'swagger_helper'
 
-RSpec.describe 'api/users_conroller', type: :request do
+describe 'Users API' do
+  path '/users' do
+    post 'creates a user' do
+      tags 'Create a new user'
+      consumes 'application/json', 'application/xml'
+      parameter name: :user, in: :body, schema: {
+        type: :object,
+        properties: {
+          name: { type: :string }
+        },
+        required: ['name']
+      }
+      response '201', 'user created' do
+        let(:user) { { name: 'Savannah' } }
+        run_test!
+      end
+
+      response '422', 'invalid request' do
+        let(:user) { { name: 02 } }
+        run_test!
+      end
+    end
+  end
+  path '/users/{id}' do
+    get 'Retrieves a user' do
+      tags 'Find a user'
+      produces 'application/json', 'application/xml'
+      parameter name: :id, in: :path, type: :string
+
+      response '200', 'user found' do
+        schema type: :object,
+               properties: {
+                 id: { type: :integer },
+                 name: { type: :string }
+               },
+               required: %w[id name]
+
+        let(:id) { User.create(name: 'Savannah').id }
+        run_test!
+      end
+
+      response '404', 'user not found' do
+        let(:id) { 'invalid' }
+        run_test!
+      end
+    end
+  end
 end
