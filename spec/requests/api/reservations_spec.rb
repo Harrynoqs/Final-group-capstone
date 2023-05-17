76515eedbc3e252d4ef062747ce2726f4258d4d8
Soulemanou Kunchiefuh Babanou <kunchiefuh@gmail.com>
2023-05-17 14:43:32 +0200

    fixed linter errors

--- spec/requests/api/reservations_spec.rb
+++ spec/requests/api/reservations_spec.rb
@@ -8,26 +8,26 @@ describe 'Reservations API' do
       parameter name: :reservation, in: :body, schema: {
         type: :object,
         properties: {
-         city: { type: :string },
-         duration_of_test_drive: { type: :integer },
+          city: { type: :string },
+          duration_of_test_drive: { type: :integer },
           date_of_reservation: { type: :string },
           user_id: { type: :integer },
           twowheeler_id: { type: :integer }
 
         },
-        required: %w[city,  duration_of_test_drive, date_of_reservation,  user_id, twowheeler_id]
+        required: %w[city, duration_of_test_drive, date_of_reservation, user_id, twowheeler_id]
       }
 
       response '201', 'reservation created' do
         let(:reservation) do
-          { city: 'Milan', date_of_reservation: '2023-05-17 00:00:00', duration_of_test_drive: 130,  user_id: 1, twowheeler_id: 1 }
+          { city: 'Milan', date_of_reservation: '2023-05-17 00:00:00', duration_of_test_drive: 130, user_id: 1, twowheeler_id: 1 }
         end
         run_test!
       end
 
       response '422', 'invalid request' do
         let(:reservation) do
-           { city: 'Milan', duration_of_test_drive: 130,  user_id: 1, twowheeler_id: 1 }
+          { city: 'Milan', duration_of_test_drive: 130, user_id: 1, twowheeler_id: 1 }
         end
         run_test!
       end
